4744f25e83d69f7de046b14d480e9c97cf82f5cd
arkady <arkady@sec.do>
2016-03-24 14:13:00 +0200

    Use dictionary, drop ActionChains(), 60s timeouts

--- synel_recorder.py
+++ synel_recorder.py
@@ -36,12 +36,12 @@ def is_alert_present(wd):
 
 def push_record(wd, username, password, company, is_enter):
     wd.get("https://harmony.synel.co.il/eharmonynew")
-    wd.implicitly_wait(30)
+    wd.implicitly_wait(60)
     wd.find_element_by_name("compInput").click()
     wd.find_element_by_name("compInput").clear()
     wd.find_element_by_name("compInput").send_keys(company)
     wd.find_element_by_name("loginBtn").click()
-    wd.implicitly_wait(30)
+    wd.implicitly_wait(60)
     wd.find_element_by_xpath("//form[@id='loginHarmonyDB']/div[6]/input").click()
     wd.find_element_by_xpath("//form[@id='loginHarmonyDB']/div[6]/input").clear()
     wd.find_element_by_xpath("//form[@id='loginHarmonyDB']/div[6]/input").send_keys(username)
@@ -49,21 +49,16 @@ def push_record(wd, username, password, company, is_enter):
     wd.find_element_by_name("password").clear()
     wd.find_element_by_name("password").send_keys(password)
     wd.find_element_by_xpath("//form[@id='loginHarmonyDB']//button[.='LOGIN']").click()
-    wd.implicitly_wait(30)
-    
-    if (is_enter):
-        wd.find_element_by_xpath("//div[@class='controls']/input[1]").click()
-        ActionChains(wd).double_click(wd.find_element_by_id("overlayReportingControlDiv")).perform()
-    else:
-        wd.find_element_by_xpath("//div[@class='controls']/input[2]").click()
-        ActionChains(wd).double_click(wd.find_element_by_id("overlayReportingControlDiv")).perform()
-        
-    wd.implicitly_wait(3)
+    wd.implicitly_wait(60)
+
+    buttons = {True: "//div[@class='controls']/input[1]", False: "//div[@class='controls']/input[2]"}    
+    wd.find_element_by_xpath(buttons[is_enter]).click()
+    wd.implicitly_wait(5)
     wd.find_element_by_id("btnOk").click()
-    wd.implicitly_wait(3)
+    wd.implicitly_wait(5)
     #wd.find_element_by_xpath("//form[@id='loginHarmonyDB']//button[.='LOGIN']").click()
     wd.find_element_by_link_text("(Logout)").click()
-    wd.implicitly_wait(3)
+    wd.implicitly_wait(5)
         
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Synel recorder')
@@ -71,17 +66,17 @@ if __name__ == '__main__':
     logger = logging.getLogger('synel')
     logger.setLevel(logging.INFO)    
 
-    is_enter = False
-    if ("enter" in arguments):
-        is_enter = True;
+    is_enter = arguments["enter"]
     company = arguments["--company"]
     password = arguments["--password"]
     username = arguments["--user"]
     
-    logger.info("Record '{0}' for user '{1}', company '{2}'".format({True:"ENTER", False:"EXIT"}[is_enter], username, company))
+    logger.info("Record '{0}' for user '{1}', company '{2}'".format(
+        {True:"ENTER", False:"EXIT"}[is_enter], 
+        username, company))
 
     wd = WebDriver()
-    wd.implicitly_wait(3)
+    wd.implicitly_wait(60)
     
     try:
         push_record(wd, username, password, company, is_enter)
